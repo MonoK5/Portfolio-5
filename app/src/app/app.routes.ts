fd4eb46b2fcc871e93d48293acea98577e29ec32
khosatho <monokhosatho@gmail.com>
2025-03-17 16:03:34 +0200

    added about and projects codes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -3,10 +3,10 @@ import { LandingComponent } from './Pages /landing/landing.component';
 import { AboutComponent } from './Pages /about/about.component';
 import { ContactComponent } from './Pages /contact/contact.component';
 import { SkillsComponent } from './Pages /skills/skills.component';
-import { ProjectsComponent } from './Pages /projects/projects.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
-{ path: '', component: LandingComponent },
+{ path: 'landing', component: LandingComponent },
 { path: 'about', component: AboutComponent },
 { path: 'contact', component: ContactComponent},
 { path: 'skills', component: SkillsComponent},
