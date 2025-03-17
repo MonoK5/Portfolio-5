745b3f225877a6d3b455a803bff194a1ccaa663d
khosatho <monokhosatho@gmail.com>
2025-03-17 10:34:10 +0200

    added routes

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './Pages /landing/landing.component';
+import { AboutComponent } from './Pages /about/about.component';
+import { ContactComponent } from './Pages /contact/contact.component';
+import { SkillsComponent } from './Pages /skills/skills.component';
+import { ProjectsComponent } from './Pages /projects/projects.component';
 
 export const routes: Routes = [
-{ path: 'landing', component: LandingComponent },
+{ path: '', component: LandingComponent },
+{ path: 'about', component: AboutComponent },
+{ path: 'contact', component: ContactComponent},
+{ path: 'skills', component: SkillsComponent},
+{path:'projects', component: ProjectsComponent}
 
 ];
