18239a351a156111407f5773249325ed9e89e123
khosatho <monokhosatho@gmail.com>
2025-03-18 16:00:19 +0200

    added projetets descriptions

--- app/src/app/Pages /about/about.component.ts
+++ app/src/app/Pages /about/about.component.ts
@@ -4,7 +4,7 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [NavComponent,RouterLink],
+  imports: [NavComponent,],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
